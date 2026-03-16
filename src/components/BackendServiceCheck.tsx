2dc8b41ec1608216e6eec67de65ca9028aad8f99
lambda-z <18048587325@163.com>
2026-03-16 20:24:21 +0800

    added BackendServiceCheck

---
+++ src/components/BackendServiceCheck.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+export function BackendServiceCheck() {
+
+
+    return (
+        <div>
+            BackendServiceCheck
+        </div>
+    )
+
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { BackendServiceCheck } from "./components/BackendServiceCheck";
 import {ScallionLocalStorageUtil} from "./utils/storage";
 
 export { Button } from './components/Button';
@@ -5,6 +6,8 @@ export { ScallionButton } from './components/ScallionButton';
 export {request} from './utils/request';
 export {DRFApi} from './drf-api/DRFApi';
 
+
 export {
     ScallionLocalStorageUtil,
+    BackendServiceCheck,
 }
