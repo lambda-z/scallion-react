26c63a4df28725da6557b80d8b7530dfa84c7da2
lambda-z <18048587325@163.com>
2026-03-16 21:07:06 +0800

    added BackendServiceCheck

--- src/components/BackendServiceCheck.tsx
+++ src/components/BackendServiceCheck.tsx
@@ -1,14 +1,59 @@
 'use client';
 
+import {DRFApi} from "../drf-api/DRFApi.ts";
+import {DRFServiceImpl} from "../drf-api/service.ts";
+import {useEffect, useState} from "react";
+import {request} from "../utils/request";
 
+class BackendServiceMetaApi extends DRFApi {
+
+    URL = '/health';
+
+    async getHealth() {
+        return await request.get(this.URL, {});
+    }
+
+}
+
+class BackendServiceImpl extends DRFServiceImpl {
+    api = new BackendServiceMetaApi();
+
+    async checkHealth(){
+
+        let ret = {}
+        this.api.getHealth().then(
+            (resp) => {
+                ret = resp?.data;
+            }
+        ).catch(
+            (error) => {
+                console.error('BackendService Connect Error: ', error)
+                ret = {service_name: null, status: "error", message: "No Service Connect."}
+            }
+        )
+
+        return ret;
+    }
+}
 
 
 export function BackendServiceCheck() {
 
+    const service = new BackendServiceImpl();
+    const [data, setData] = useState({service_name: '', status: '', message: ''});
+
+    const checkHealth = async () => {
+        const resp = await service.checkHealth();
+        setData({message: "", service_name: "", status: "", ...resp})
+    }
+
+    useEffect(() => {
+        checkHealth().then();
+    }, []);
 
     return (
         <div>
-            BackendServiceCheck
+            BackendServiceCheck: {data?.service_name}
         </div>
     )
 
--- usage.md
+++ usage.md
@@ -9,3 +9,7 @@ You can config it in `.env` file.
 ```text
 NEXT_PUBLIC_BACKEND_SERVICE_URL=http://127.0.0.1:8000
 ```
+
+
+## DRFService
+
